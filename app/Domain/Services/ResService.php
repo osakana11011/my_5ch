3200ff3112387b94732daa39114df7fd403652e7
osakana11011 <taka.knk.11011@gmail.com>
2020-08-03 00:17:58 +0900

    (#5) 上限以上のレスが投稿されないように制御を追加

--- app/Domain/Services/ResService.php
+++ app/Domain/Services/ResService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Domain\Services;
 
 use App\Domain\Repositories\IResRepository;
+use App\Domain\Models\Entities\Thread;
 use App\Domain\Models\Entities\Res;
 use App\Domain\Models\ValueObject\Thread\ThreadID;
 use App\Domain\Models\ValueObject\Res\ResContent;
@@ -29,6 +30,10 @@ final class ResService
     public function createRes(int $threadID, ?string $submitterName, string $content): void
     {
         $_threadID = new ThreadID($threadID);
+        if (count($this->resRepository->getByThreadID($_threadID)) > Thread::MAX_RES_NUMBER) {
+            return;
+        }
+
         $resSubmitterName = new ResSubmitterName($submitterName);
         $resContent = new ResContent($content);
         $res = new Res($_threadID, $resContent, $resSubmitterName);
